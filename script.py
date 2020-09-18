87114535839ce08bfe4cdcdb43032d2cdc3d1ab4
Eugene Birondo <jbirondo@gmail.com>
2020-09-17 23:25:50 -0700

    res.ok block

--- script.py
+++ script.py
@@ -10,3 +10,26 @@ except ImportError:
 def get_fantasy_points(player, pos):
     if player.get('position') == pos:
         return player.get('fantasy_points').get('ppr')
+
+
+pos = 'WR'
+year = '2019'
+week = 1
+
+res = requests.get(
+    'https://www.fantasyfootballdatapros.com/api/players/{0}/{1}'.format(year, week))
+
+if res.ok:
+    print('Season {0}, week {1} VOR for {2}s'.format(year, week, pos))
+    print('-'*40)
+
+    data = res.json()
+
+    wr_fantasy_points = [get_fantasy_points(player, pos) for player in data]
+
+    wr_fantasy_points = list(
+        filter(lambda x: x is not None, wr_fantasy_points))
+
+    def mean(x): return sum(x)/len(x)
+
+    replacement_value = mean(wr_fantasy_points)
