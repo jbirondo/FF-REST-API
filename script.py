30ace827ebad96a3550a51d59de708f446f96b31
Eugene Birondo <jbirondo@gmail.com>
2020-09-17 23:25:11 -0700

    Create get fantasy points function

--- script.py
+++ script.py
@@ -5,3 +5,8 @@ try:
 except ImportError:
     sys.exit(
         'requests was not properly installed. Try again. Are you sure you are in a venv?')
+
+
+def get_fantasy_points(player, pos):
+    if player.get('position') == pos:
+        return player.get('fantasy_points').get('ppr')
